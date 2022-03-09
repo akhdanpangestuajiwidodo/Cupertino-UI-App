1733ee5f93c7f4a9639f5bf4a7e861d5d1fa701e
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-10 05:58:33 +0700

    refactor home_page.dart name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cupertino_ui_app/screens/HomePage.dart';
+import 'package:cupertino_ui_app/screens/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
