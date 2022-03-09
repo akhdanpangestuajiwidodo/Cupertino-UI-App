7d42080e16a2477ed532e7920956411b22ed9ef6
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2022-03-10 06:07:30 +0700

    create content of tabBar and tabBuilder

--- lib/screens/new_home_page.dart
+++ lib/screens/new_home_page.dart
@@ -6,6 +6,32 @@ class NewHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(), tabBuilder: (context, index) {});
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.news),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.search),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings),
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          switch(index){
+            case 0:
+            return FeedsPage();
+            case 1:
+              return SearchPage();
+            case 2:
+              return SettingsPage();
+            default:
+              return Center(
+                child: Text('Page not found!'),
+              );
+          }
+        });
   }
 }
